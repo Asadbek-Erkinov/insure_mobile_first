30d3e7f102c1bf20d9ec85f77a8b601df1f92a21
Asadbek-Erkinov <asadbekeekinovg777@gmail.com>
2024-09-27 13:28:07 +0500

    new commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var elSiteHeader = document.querySelector(".site-header-js");
+var elShowMenuBtn = elSiteHeader.querySelector(".js-show-menu-btn");
+
+elShowMenuBtn.addEventListener("click", function() {
+
+elSiteHeader.classList.toggle("show-menu");
+
+
+});
